19313691cc4a291763104b0a9c5eef87844f8e0c
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2019-12-02 22:23:16 -0600

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import { Link } from "gatsby"
+
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <article class="athelas">
+    <div
+      class="vh-100 dt w-100 tc bg-dark-gray white cover"
+      style={{
+        background: "url(../../cover-2.jpg) no-repeat center",
+      }}
+    >
+      <div class="dtc v-mid">
+        <header class="white-70">
+          <h2 class="f3 fw1 ttu tracked mb2 lh-title">Summer MMXX</h2>
+          <h2 class="f4 fw1 lh-title">Morton Arboretum</h2>
+        </header>
+        <h1 class="f1 f-headline-l fw1 i white-60">CELINNE & JAY</h1>
+        <blockquote class="ph0 mh0 measure f4 lh-copy center">
+          <p class="fw1 white-70">Coming Soon...</p>
+        </blockquote>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </article>
 )
 
 export default IndexPage
